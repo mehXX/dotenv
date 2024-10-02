b248bc8a2dde64398b0f817071be377418d39d4f
Nikita Naumov <n.naumov@uzum.com>
2024-10-03 00:31:03 +0500

    sync envs

--- settings/bin/karabiner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd karabiner || return
-yarn run build  && \
-rm -f "$HOME/.config/karabiner/karabiner.json" && \
-cp "$HOME/settings/karabiner/karabiner.json" "$HOME/.config/karabiner"
